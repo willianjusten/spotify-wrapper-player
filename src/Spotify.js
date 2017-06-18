6ae099124d7c3c6991239b1f159ef84954cf55ff
Willian Justen <willianjustenqui@gmail.com>
2017-06-18 20:55:48 -0300

    Add AlbumList to interface

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'YOUR_TOKEN_HERE'
+  token: 'BQDoAZEvltPmCfS5t08f1zq4ynnrU-2BfQ7PgUdfgZdrXF-bblvzUkaS2RY15eiV9ohPY5igkMTmqYJr8b9qQRwtp8kDdV-4zlDuXjAuKYmBmMayykeErqe1E4T0M8sSVLJm2URt-CT4hpv5RCE',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Spotify Player!');
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('Incubus');
+const albumList = document.getElementById('album-list');
+
+albums
+  .then(data => renderAlbums(data.albums.items, albumList));
