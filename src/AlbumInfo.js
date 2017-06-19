4542459b6dd442244f937222f597d2a00564e310
Willian Justen <willianjustenqui@gmail.com>
2017-06-18 21:22:10 -0300

    Add AlbumInfo component

---
+++ src/AlbumInfo.js
@@ -0,0 +1,13 @@
+function createMarkup(data) {
+  return (`
+    <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
+    <p class="album-title">${data.name}</p>
+    <p class="album-artist">${data.artists[0].name}</p>
+    <p class="album-counter">${data.tracks.total} Músicas</p>
+  `);
+}
+
+export default function renderAlbumInfo(data, element) {
+  const markup = createMarkup(data);
+  element.innerHTML = markup;
+};
---
+++ tests/AlbumInfo.spec.js
@@ -0,0 +1,47 @@
+import 'jsdom-global/register';
+import { expect } from 'chai';
+
+import renderAlbumInfo from '../src/AlbumInfo';
+
+describe('AlbumInfo', () => {
+
+  const data = {
+    "album_type" : "album",
+    "artists" : [ {
+      "name" : "Incubus",
+    } ],
+    "id" : "6peEdPVO73WtgGah5sEhX4",
+    "images" : [ {
+      "height" : 640,
+      "url" : "https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757",
+      "width" : 640
+    }, {
+      "height" : 300,
+      "url" : "https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6",
+      "width" : 300
+    }, {
+      "height" : 64,
+      "url" : "https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16",
+      "width" : 64
+    } ],
+    "name" : "The Essential Incubus",
+    "type" : "album",
+    "tracks": {
+      "total": 18
+    }
+  };
+
+  const markup = `
+    <img class="album-image" src="https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757" alt="The Essential Incubus">
+    <p class="album-title">The Essential Incubus</p>
+    <p class="album-artist">Incubus</p>
+    <p class="album-counter">18 Músicas</p>
+  `;
+
+  it('should create and append the markup given a correct data', () => {
+    const element = document.createElement('div');
+    renderAlbumInfo(data, element);
+
+    expect(element.innerHTML).to.be.eql(markup);
+  });
+});
