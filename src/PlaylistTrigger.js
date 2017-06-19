386f2baaf0360788f9e470f0fa353020d26bb75f
Willian Justen <willianjustenqui@gmail.com>
2017-06-19 01:00:49 -0300

    Add PlaylistTrigger

---
+++ src/PlaylistTrigger.js
@@ -0,0 +1,27 @@
+const albumTracks = document.getElementById('album-tracks');
+let audioObject = null;
+
+export default function playlistTrigger() {
+  albumTracks.addEventListener('click', (e) => {
+    const target = e.target.parentNode;
+
+    if(target.classList.contains('active')) {
+      audioObject.pause();
+    } else {
+      if(audioObject) {
+        audioObject.pause();
+      }
+      audioObject = new Audio(target.getAttribute('data-track-preview'));
+      audioObject.play();
+      target.classList.add('active');
+
+      audioObject.addEventListener('ended', () => {
+        target.classList.remove('active');
+      });
+
+      audioObject.addEventListener('pause', () => {
+        target.classList.remove('active');
+      });
+    }
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import searchEnterTrigger from './SearchTrigger';
 import selectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
 searchEnterTrigger();
 selectAlbumTrigger();
+playlistTrigger();
