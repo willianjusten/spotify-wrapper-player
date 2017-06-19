160eabb560b756c929b170891f9fad86c588a8d1
Willian Justen <willianjustenqui@gmail.com>
2017-06-18 22:23:15 -0300

    Add AlbumTracks to layout

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
 
 const albums = spotify.search.albums('Incubus');
 const albumList = document.getElementById('album-list');
 
-const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
+const album = spotify.album.getAlbum('2i6nd4FV6y7K9fln6eelmR');
 const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-tracks');
 
 albums
   .then(data => renderAlbums(data.albums.items, albumList));
 
 album
-  .then(data => renderAlbumInfo(data, albumInfo));
+  .then(data => renderAlbumInfo(data, albumInfo))
+  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
