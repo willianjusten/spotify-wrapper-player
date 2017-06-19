b9d091f7b9c07934b137597333bd36d878bf9da8
Willian Justen <willianjustenqui@gmail.com>
2017-06-18 23:36:36 -0300

    Add data-album-id to AlbumList

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,10 +1,10 @@
 function createMarkup(data) {
   return data.map(album => `
-    <div class="list-item">
-      <img src="${album.images[2].url}" alt="${album.name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${album.name}</p>
-        <p class="list-subtitle">${album.artists[0].name}</p>
+    <div class="list-item" data-album-id="${album.id}">
+      <img src="${album.images[2].url}" alt="${album.name}" class="list-image" data-album-id="${album.id}">
+      <div class="list-description" data-album-id="${album.id}">
+        <p class="list-title" data-album-id="${album.id}">${album.name}</p>
+        <p class="list-subtitle" data-album-id="${album.id}">${album.artists[0].name}</p>
       </div>
     </div>`).join('');
 }
--- tests/AlbumList.spec.js
+++ tests/AlbumList.spec.js
@@ -80,27 +80,27 @@ describe('AlbumList', () => {
   ];
 
   const markup = `
-    <div class="list-item">
-      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${data[0].name}</p>
-        <p class="list-subtitle">${data[0].artists[0].name}</p>
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
       </div>
     </div>`;
 
   const markup2 = `
-    <div class="list-item">
-      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${data[0].name}</p>
-        <p class="list-subtitle">${data[0].artists[0].name}</p>
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
       </div>
     </div>
-    <div class="list-item">
-      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${data[0].name}</p>
-        <p class="list-subtitle">${data[0].artists[0].name}</p>
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
       </div>
     </div>`;
 
