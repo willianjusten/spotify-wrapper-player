67772291407c4364b7e46ae40b7f0543eda36478
Willian Justen <willianjustenqui@gmail.com>
2017-06-18 21:36:22 -0300

    Add AlbumInfo to layout

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQDoAZEvltPmCfS5t08f1zq4ynnrU-2BfQ7PgUdfgZdrXF-bblvzUkaS2RY15eiV9ohPY5igkMTmqYJr8b9qQRwtp8kDdV-4zlDuXjAuKYmBmMayykeErqe1E4T0M8sSVLJm2URt-CT4hpv5RCE',
+  token: 'BQAJD4JH3Da4O_c_LacJCcqJf9xRuoqnAerqclY5Ye1fQRTdIrJYVG8X_B2z-UfniDi3hDik8fu0wQ9g840Hlxjw_YyO_kHovmK3iga0G0M_bB1-GI8DWJCC-qMkB7sFhkws1FUCdiKC93NxPec',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Incubus');
 const albumList = document.getElementById('album-list');
 
+const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
+const albumInfo = document.getElementById('album-info');
+
 albums
   .then(data => renderAlbums(data.albums.items, albumList));
+
+album
+  .then(data => renderAlbumInfo(data, albumInfo));
